b364d06715379ad43484e18ca42843bf87335bf0
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-01-26 00:25:48 +0600

    solve two js regEx problem

---
+++ js_topics/regExPractice.js
@@ -0,0 +1,13 @@
+//  ----- valid password ------
+
+// const regEx = /^[A-Z][0-9]{3}[a-z]/g;
+// console.log(regEx.test("A123b"));
+
+
+//  ----- valid email ------
+
+// const validPhoneNum = /(\+?880)-[0-9]{3}-[0-9]{4}/g
+// const text = "Call me at +880-123-4567 or 880-987-6543"
+// console.log(text.match(validPhoneNum))
+
+// ------- Exercise 3: Find Dates in Different Formats  ------
