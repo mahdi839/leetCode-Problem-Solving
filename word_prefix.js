0aa66a137fe95260c1639717d3011e4fa847ade5
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-02-13 02:44:22 +0600

    solve leetCode problem 1445

--- word_prefix.js
+++ word_prefix.js
@@ -1,9 +1,27 @@
-let sentence = "i love eating burger"
-let  searchWord = "burg"
-let converted = sentence.split(" ");
+var isPrefixOfWord = function(sentence, searchWord) {
+    let searchWordIndex = sentence.indexOf( searchWord)
+    let converted = sentence.split(" ");
+    let resultArr = ""
+    for(word of converted){
+        if(sentence.indexOf(word) === searchWordIndex){
+           resultArr = word
+        }
+    }
+   
+    return converted.indexOf(resultArr)+1;
+};
 
-for(word of converted){
-    console.log(word)
-}
-let val = sentence.search(searchWord)
-console.log(val)
+
+let sentence = "i love burger eating burger amar"
+let searchWord = "burg"
+console.log(isPrefixOfWord(sentence,searchWord))
+// console.log("index" + sentence.indexOf( searchWord))
+// let converted = sentence.split(" ");
+
+// for(word of converted){
+//    if(sentence.indexOf(word) == 14){
+//        console.log("result" + word)
+//    }
+// }
+// let val = sentence.search(searchWord)
+// console.log(val)
