a40d907128610ecf8930c4411596be4f9f00e191
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-04-03 09:22:09 +0600

    solve leetCode destination city prolem. problem number is 1436

---
+++ destinationCity.js
@@ -0,0 +1,22 @@
+var destCity = function(paths) {
+   let mapPaths = new Map()
+   for(let i=0;i<paths.length;i++){
+    paths[i].forEach((path)=>{
+      mapPaths.set(path,mapPaths.get(path)+1||1)
+    })
+   }
+
+   let result = ''
+   mapPaths.forEach((value,key)=>{
+    for(let i=0;i<paths.length;i++){
+        if(value === 1 && key ===paths[i][1]){
+             
+             result = paths[i][1]
+        }
+    }
+   })
+   return result;
+};
+
+let paths = [["pYyNGfBYbm","wxAscRuzOl"],["kzwEQHfwce","pYyNGfBYbm"]]
+console.log(destCity(paths))
