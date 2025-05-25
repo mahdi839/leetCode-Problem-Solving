a1f329f942cba3e02adc4d2836fee34dde40653b
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-05-26 01:11:19 +0600

    solved leetCode 2053rd problem

--- kth_distinct_str.js
+++ kth_distinct_str.js
@@ -1,9 +1,27 @@
 function getStr (arr,k){
    let CountArr = new Map();
    for (let i=0; i<arr.length; i++){
-     CountArr.set(arr[i],{count:CountArr.get(arr[i]) +1|| 1,index:i})
+     if(CountArr.has(arr[i])){
+       let existing = CountArr.get(arr[i])
+       CountArr.set(arr[i],{count:existing.count+1,index:existing.index})
+     }else{
+       CountArr.set(arr[i],{count:1,index:i})
+     }
    }
-   console.log(CountArr)
+   const kthVal = []
+  CountArr.forEach((value,key) => {
+    
+      if(value.count ===1){
+        kthVal.push(key)
+      }
+  });
+  if(kthVal.length<k){
+    return ""
+  }
+ else{
+    return kthVal[k-1]
+  }
+  
 }
 let arr = ["d","b","c","b","c","a"] 
 let k = 2
